d84860b95ba2e45f4d66b4cae09f1f4d13848ede
Ivanchrist225 <avoldigo2@gmail.com>
2025-06-04 21:05:46 +0000

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100051956832579"];
+		const permission = ["61551757747742"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
